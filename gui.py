b64de1d7290386e8f6b867fbcec9dd19c24addd5
kira-segenchuk <relatively.kira@gmail.com>
2021-12-14 00:18:19 -0500

    Changed visual for 2x2 game

--- gui.py
+++ gui.py
@@ -21,22 +21,31 @@ class Mw_vis_2x2():
         y1 = coords[1] + r
         return self.canvas.create_oval(x0, y0, x1, y1, fill="red" if player else "blue")
 
-    def map_coords(self, strat):
-        return (strat[0]*300+100 , strat[1]*300+100)
+    # def map_coords(self, strat):
+    #     return (strat[0]*300+100 , strat[1]*300+100)
+
+    # def main_loop(self):
+    #     if self.i < self.game.num_iters:
+    #         self.create_circle(self.map_coords(self.game.x_strats[self.i]), 15, True)
+    #         self.create_circle(self.map_coords(self.game.y_strats[self.i]), 15, False)
+    #         self.canvas.after(100, self.main_loop)
+    #         self.i += 1
+
+    def map_coords(self, x_strat, y_strat):
+        return (x_strat[0] * 300 + 100, y_strat[0] * 300 + 100)
 
     def main_loop(self):
         if self.i < self.game.num_iters:
-            self.create_circle(self.map_coords(self.game.x_strats[self.i]), 15, True)
-            self.create_circle(self.map_coords(self.game.y_strats[self.i]), 15, False)
+            self.create_circle(self.map_coords(self.game.x_strats[self.i], self.game.y_strats[self.i]), 5, True)
             self.canvas.after(100, self.main_loop)
             self.i += 1
 
 if __name__ == "__main__":
     game_mat = np.array([[1, -1], [-1, 1]])
     eta = 0.1
-    init_strategy_x = np.array([0.999, 0.001])
-    init_strategy_y = np.array([0.999, 0.001])
-    game = mw_zero_sum.Mw_zero_sum(game_mat, eta, init_strategy_x, init_strategy_y)
+    init_strategy_x = np.array([0.51, 0.49])
+    init_strategy_y = np.array([0.51, 0.49])
+    game = mw_zero_sum.Mw_zero_sum(game_mat, eta, init_strategy_x, init_strategy_y, 1000)
 
     print(game.x_strats)
     print(game.y_strats)

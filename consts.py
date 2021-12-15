fe36468bd5747b9bc899a9de045bbfa93401d2fe
Zoli <zcc6@cornell.edu>
2021-12-15 10:18:55 -0500

    clean up 2D visual, and begin work on 3D visual

---
+++ consts.py
@@ -0,0 +1,7 @@
+PLAYER1_COLOR = "red"
+PLAYER2_COLOR = "blue"
+FRAME_RATE = 20
+POINT_RADIUS = 2
+TAIL_LENGTH = 8
+COLORS = ["#" + str(hex(i))[2:].zfill(2) + "0000" for i in range(0, 256, 8)]
+NEW_COLOR = "#ff0000"
